f14142587764ecc662b21c8a1c84266f86d3a469
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2015-11-10 05:42:24 -0200

    One spare

--- src/bowling.cpp
+++ src/bowling.cpp
@@ -2,14 +2,22 @@
 
 size_t bowling( std::string const &scores )
 {
-    int acc = 0;
-    for(auto const& letter : scores)
+    int acc        = 0;
+    char last_play = 0;
+
+    for( auto const &play : scores )
     {
-      
+        if( play != '/' )
+        {
+            acc += play - '0';
+        }
+        else
+        {
+            acc += 10 - last_play;
+        }
+
+        last_play = play - '0';
     }
-    if(scores.size() > 1) 
-    {
-      return 4;
-    }
-    return 3;
+
+    return acc;
 }
--- tests/bowling_test.cpp
+++ tests/bowling_test.cpp
@@ -7,12 +7,22 @@ TEST( BowlingKata, one_ball )
     ASSERT_THAT( bowling( "3" ), Eq( 3 ) );
 }
 
-TEST(BowlingKata, two_bolls )
+TEST( BowlingKata, two_balls )
 {
-  ASSERT_THAT(bowling("31"), Eq(4));
+    ASSERT_THAT( bowling( "31" ), Eq( 4 ) );
 }
 
-TEST(BowlingKata, two_bolls_3_2)
+TEST( BowlingKata, two_balls_3_2 )
 {
-  ASSERT_THAT(bowling("32"), Eq(5));
+    ASSERT_THAT( bowling( "32" ), Eq( 5 ) );
+}
+
+TEST( BowlingKata, five_balls )
+{
+    ASSERT_THAT( bowling( "32791" ), Eq( 22 ) );
+}
+
+TEST( BowlingKata, one_spare )
+{
+    ASSERT_THAT( bowling( "3/" ), Eq( 10 ) );
 }
