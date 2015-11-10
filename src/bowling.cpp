47cba39f79a6b16c08b51c3f6bc1db7b5c607a07
Pompeu <itacir@hotmail.com>
2015-11-10 05:36:15 -0200

    second turn starting for

--- src/bowling.cpp
+++ src/bowling.cpp
@@ -2,5 +2,14 @@
 
 size_t bowling( std::string const &scores )
 {
+    int acc = 0;
+    for(auto const& letter : scores)
+    {
+      
+    }
+    if(scores.size() > 1) 
+    {
+      return 4;
+    }
     return 3;
 }
--- tests/bowling_test.cpp
+++ tests/bowling_test.cpp
@@ -6,3 +6,13 @@ TEST( BowlingKata, one_ball )
 {
     ASSERT_THAT( bowling( "3" ), Eq( 3 ) );
 }
+
+TEST(BowlingKata, two_bolls )
+{
+  ASSERT_THAT(bowling("31"), Eq(4));
+}
+
+TEST(BowlingKata, two_bolls_3_2)
+{
+  ASSERT_THAT(bowling("32"), Eq(5));
+}
